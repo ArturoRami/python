c1a5e2cb17c726af33ae3daaf995d2130b021eba
unknown <2020.arturo.ramirez@bycenj.edu.mx>
2022-06-17 10:35:24 -0500

    first commit

---
+++ tiposvariables.py
@@ -0,0 +1,23 @@
+#string - cadena de textos
+
+nombre = ("Arturo")
+print(nombre)
+
+#Enteros - números sin decimales
+edad = 23
+print(edad)
+
+# Flotantes - números con decimales
+
+pi = 3.1416
+edad = float(edad) #Casteo, transformar una variable a otra
+
+#Usa type para saber el tipo de variable que se usa
+print (type(nombre),type(edad))
+
+#Bool -Booleano, respuestas de si o no. 
+legustas = False
+
+tegusto = True
+
+print (tegusto)
